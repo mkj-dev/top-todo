68a2e9c6ee082d9578bb4294ad50d5ee5bf0d14c
mstach0wicz <mikolaj.stachowicz0@gmail.com>
2022-12-29 21:32:20 +0100

    Add an event listener to #close-form-btn to hide the form

--- scripts/handle_form_display.js
+++ scripts/handle_form_display.js
@@ -1,6 +1,11 @@
 const createTodoBtn = document.getElementById('create-todo-btn');
+const closeFormBtn = document.getElementById('close-form-btn')
 const todoForm = document.getElementById('todo-form');
 
 createTodoBtn.addEventListener('click', () => {
     todoForm.style.display = 'block';
+})
+
+closeFormBtn.addEventListener('click', () => {
+    todoForm.style.display = 'none';
 })
