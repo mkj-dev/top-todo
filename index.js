24970e56275cefbc6151b56260b811550d03bd0d
mstach0wicz <mikolaj.stachowicz0@gmail.com>
2022-12-29 23:44:06 +0100

    Add deleting tasks from the local storage

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 import Task from "./modules/Task.js";
 
+// Form elements
+const taskTitle = document.querySelector("#title");
+const taskDesc = document.querySelector("#description");
+const taskDeadline = document.querySelector("#deadline");
+const addTaskBtn = document.querySelector("#addTask");
 const tasks = document.querySelector("#tasks");
+const taskArray = [];
 
 // Retrieve the taskArray from local storage
 const storedTasks = localStorage.getItem("tasks");
@@ -8,22 +14,22 @@ const storedTasks = localStorage.getItem("tasks");
 if (storedTasks) {
   const parsedTasks = JSON.parse(storedTasks);
 
-  parsedTasks.forEach(task => {
+  parsedTasks.forEach((task, index) => {
     const taskElement = document.createElement("div");
-    taskElement.innerHTML = `<h3>${task.title}</h3><p>${task.description}</p><p>${task.deadline}</p>`;
+    taskElement.innerHTML = `<h3>${task.title}</h3><p>${task.description}</p><p>${task.deadline}</p><button class="delete-task-btn">Delete</button>`;
     tasks.appendChild(taskElement);
+  
+    const deleteBtn = taskElement.querySelector(".delete-task-btn");
+    deleteBtn.addEventListener("click", () => {
+      // Remove the task from the taskArray
+      parsedTasks.splice(index, 1);
+      // Save the updated taskArray in local storage
+      localStorage.setItem("tasks", JSON.stringify(parsedTasks));
+      // Remove the task element from the tasks div
+      taskElement.remove();
+    });
   });
 }
-
-const taskArray = [];
-
-// Form elements
-const taskTitle = document.querySelector("#title");
-const taskDesc = document.querySelector("#description");
-const taskDeadline = document.querySelector("#deadline");
-
-// Task button
-const addTaskBtn = document.querySelector("#addTask");
 
 addTaskBtn.addEventListener("click", () => {
 
