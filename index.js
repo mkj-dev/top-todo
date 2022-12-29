dadaecd5719f36862fd9e667222a269e9b2a7766
mstach0wicz <mikolaj.stachowicz0@gmail.com>
2022-12-29 23:00:13 +0100

    Add retrieving tasks from the local storage

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
 import Task from "./modules/Task.js";
 
 const tasks = document.querySelector("#tasks");
+
+// Retrieve the taskArray from local storage
+const storedTasks = localStorage.getItem("tasks");
+
+if (storedTasks) {
+  const parsedTasks = JSON.parse(storedTasks);
+
+  parsedTasks.forEach(task => {
+    const taskElement = document.createElement("div");
+    taskElement.innerHTML = `<h3>${task.title}</h3><p>${task.description}</p><p>${task.deadline}</p>`;
+    tasks.appendChild(taskElement);
+  });
+}
+
 const taskArray = [];
 
 // Form elements
