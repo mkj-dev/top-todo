90c23099959ede5adc6a1b33cb9e8d551fd54a5b
mstach0wicz <mikolaj.stachowicz0@gmail.com>
2023-02-10 09:18:22 +0100

    Create a script that calculates the time remaining until deadline

---
+++ scripts/deadline_countdown.js
@@ -0,0 +1,47 @@
+// Get stored todos and parse them into JavaScript object
+const storedTodos = localStorage.getItem("todos");
+const parsedTodos = JSON.parse(storedTodos);
+
+const deadlineDates = [];
+
+parsedTodos.forEach(deadline => {
+    deadlineDates.push(deadline['deadline'].split('-'));
+});
+
+// Function that calculates the difference between dates and displays it as a countdown
+function getTimeRemaining(endtime) {
+    var t = Date.parse(endtime) - Date.parse(new Date());
+    var seconds = Math.floor((t / 1000) % 60);
+    var minutes = Math.floor((t / 1000 / 60) % 60);
+    var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+    var days = Math.floor(t / (1000 * 60 * 60 * 24));
+    return {
+        'total': t,
+        'days': days,
+        'hours': hours,
+        'minutes': minutes,
+        'seconds': seconds
+    };
+}
+
+const todoCollection = document.getElementsByClassName('todo');
+
+// Function that calculates the time remaining until a deadline for a collection of todos
+function getTimeLeftToDeadline(collection) {
+    for (let i = 0; i < collection.length; i++) {
+        const deadlineTime = getTimeRemaining(new Date(collection[i][0], collection[i][1] - 1, collection[i][2]));
+
+        const days = document.createElement('p');
+        days.setAttribute('class', 'time-left');
+        const hours = document.createElement('p');
+        hours.setAttribute('class', 'time-left');
+
+        days.innerText = `Days left: ${deadlineTime['days']}`;
+        hours.innerText = `Hours left: ${deadlineTime['hours']}`;
+
+        todoCollection[i].children[2].after(days);
+        todoCollection[i].children[3].after(hours);
+    }
+}
+
+getTimeLeftToDeadline(deadlineDates);
